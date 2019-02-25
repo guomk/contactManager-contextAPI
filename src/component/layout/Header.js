06a2b0e8b66353446d35de5a309a861280062ac2
guomukun <gmk19990312@gmail.com>
2019-02-25 16:25:37 +0800

    make navbar better

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -1,24 +1,29 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = props => {
   const { branding } = props;
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-3 py-0">
-      <div className="container">
-        <a href="/" className="navbar-brand">{branding}</a>
-      </div>
-      <ul>
-        <ul className="navbar-nav mr-auto">
-          <a href="home" className="nav-link">Home</a>
+    <nav className='navbar navbar-expand-sm navbar-dark bg-primary mb-3 py-0 '>
+      <a className='navbar-brand mb-0 h1' href='/'>
+        {branding}
+      </a>
+
+      <div className='collapse navbar-collapse'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <a className='nav-link' href='home'>
+              Home
+            </a>
+          </li>
         </ul>
-      </ul>
+      </div>
     </nav>
   );
 };
 
 Header.defaultProps = {
-  branding: "My App"
+  branding: 'My App'
 };
 
 Header.propTypes = {
